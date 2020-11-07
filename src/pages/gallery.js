896db25962fb11c0081e8d57624c54f8f200d9c2
Taylor <tdy721@gmail.com>
2020-11-07 10:40:41 -0700

    gallery order fix

--- src/pages/gallery.js
+++ src/pages/gallery.js
@@ -8,18 +8,14 @@ import Image from "gatsby-image"
 
 const SecondPage = ({ data }) => {
   const images = data.allFile.edges.sort( (a, b) => {
-    console.log( a)
-    // return a - b
-    return a.node.name.replace('Linceo-Select-','') - b.node.name.replace('Linceo-Select-','')
+    return parseInt(a.node.name.replace('Linceo-Select-','')) - parseInt(b.node.name.replace('Linceo-Select-',''))
   })
-  console.log(images)
   return (
   <Layout pageInfo={{ pageName: "Gallery", slug: "gallery" }}>
     <SEO title="Gallery" />
     <h1>Gallery</h1>
     <Row className="gallery" noGutters>
     {images.map( (image, idx) => {
-      console.log( image )
       return (<Col xs={6} sm ={4} md={3} lg={2}>
         <Image fluid={image.node.childImageSharp.fluid} />
         {/* <img src={`/LinceoSelect/${image.node.name}${image.node.ext}`} alt="" /> */}
